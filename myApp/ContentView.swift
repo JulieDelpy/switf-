7763d0c0d970e3df6f569582b5e4ff14ee7f33b0
JulieDelpy <104055253+JulieDelpy@users.noreply.github.com>
2023-03-26 21:56:37 +0800

    Add files via upload
    五子棋游戏创建

---
+++ myApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  myApp
+//
+//  Created by 陈慧琦 on 2023/3/22.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+
+    
+    var body: some View {
+        Text("hello world!")
+    }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+            
+        }
+    }
+}
---
+++ myApp/myAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by 陈慧琦 on 2023/3/22.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
